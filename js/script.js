9135f439b6d7d5cd722b07090475075f86b7f2a5
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-09-15 18:16:54 +0300

    fix syles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-document.addEventListener("click", ({ target }) => {
-  const button = target.closest(".exan-faq__question");
-  if (button) {
-    button.parentNode
-      .querySelector(".exan-faq__answer")
-      .classList.toggle("open");
-    button.parentNode.querySelector(".exan-faq__add").classList.toggle("close");
-    button.parentNode
-      .querySelector(".exan-faq__close")
-      .classList.toggle("open");
+const copyBtn = document.querySelector('.copy-btn');
+const copyInput = document.querySelector(".copy-input");
+const copyText = document.querySelector(".copy-text");
+copyBtn.addEventListener("click", getCopy);
+copyBtn.addEventListener("mouseout", getClass);
+
+  function getCopy() {
+    copyInput.select();
+    document.execCommand("copy");
+    copyInput.classList.add("copy-hidden");
+    copyText.classList.add("copy-visible");
+    
   }
-});
+ function getClass() {
+   copyInput.classList.remove("copy-hidden");
+   copyText.classList.remove("copy-visible");
+   console.log('ghbdtn')
+ }
