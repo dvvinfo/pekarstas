fa85346450b731d04ec9c4743d95986d52e65b79
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-09-20 14:47:47 +0300

    add icons unit-logo

--- js/slider_mobile.js
+++ js/slider_mobile.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".room-content-mobile__slider").owlCarousel({
     items: 1,
-    stagePadding: 30,
+    stagePadding: 40,
     loop: true,
     margin: 10,
     autoHeight: true,
