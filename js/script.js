e77bcf1c4faa5c4db3f1659de368f649c6bd8ebb
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-09-20 13:09:55 +0300

    add mobile slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const copyBtn = document.querySelector('.copy-btn');
+
+const copyBtn = document.querySelector(".copy-btn");
 const copyInput = document.querySelector(".copy-input");
 const copyText = document.querySelector(".copy-text");
 const roomRageRate = document.querySelector("#room-page-rate");
@@ -8,30 +9,28 @@ const roomLanding2CoverRating = document.querySelector(
 const roomLanding2CoverRatingGrade = document.querySelector(
   ".room-landing2-cover__rating-grade"
 );
-roomLanding2CoverRatingGrade.addEventListener('click', () => {
+
+// всплывашка Рейтинг покер-рума
+roomLanding2CoverRatingGrade.addEventListener("click", () => {
   roomLanding2CoverRating.classList.add("active");
-  roomLanding2CoverRating.style.height='480px'
+  roomLanding2CoverRating.style.height = "480px";
 });
 roomLanding2CoverRating.addEventListener("click", () => {
   roomLanding2CoverRating.classList.remove("active");
   roomLanding2CoverRating.style.height = "0";
 });
 
-
-
+// сопирование кода
 copyBtn.addEventListener("click", getCopy);
 copyBtn.addEventListener("mouseout", getClass);
 
-  function getCopy() {
-    copyInput.select();
-    document.execCommand("copy");
-    copyInput.classList.add("copy-hidden");
-    copyText.classList.add("copy-visible");
-    
-  }
- function getClass() {
-   copyInput.classList.remove("copy-hidden");
-   copyText.classList.remove("copy-visible");
-   console.log('ghbdtn')
- 
- }
+function getCopy() {
+  copyInput.select();
+  document.execCommand("copy");
+  copyInput.classList.add("copy-hidden");
+  copyText.classList.add("copy-visible");
+}
+function getClass() {
+  copyInput.classList.remove("copy-hidden");
+  copyText.classList.remove("copy-visible");
+}
---
+++ js/slider_mobile.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".room-content-mobile__slider").owlCarousel({
+    items: 1,
+    stagePadding: 30,
+    loop: true,
+    margin: 10,
+    autoHeight: true,
+  });
+});
