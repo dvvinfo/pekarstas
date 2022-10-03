8161795af0546d280de7c1313f3abe8ade7eb121
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-10-03 15:14:57 +0300

    fix

--- js/slider_mobile.js
+++ js/slider_mobile.js
@@ -17,4 +17,39 @@ $(document).ready(function () {
       owl.trigger("to.owl.carousel", [jQuery(this).index(), 300]);
     }
   );
+
+
+  var owl = $(".academy-tabs__nav-tabs"),
+    // rangeArr = [],
+    inputType = $(".slider-range__input");
+owl.owlCarousel({
+  items: 1,
+  loop: true,
+  margin: 8,
+  autoHeight: true,
+  stagePadding: 10,
+});
+//   function getIndex(event) {
+
+//   }
+owl.on('changed.owl.carousel', function(event) {
+  console.log(event.item.index);
+  inputType.val(event.item.index);
+   
+});
+$("input").on("change", function (e) {
+  e.preventDefault();
+  console.log(inputType.val());
+  // console.log(e.item.index);
+  // FIGURE OUT HOW TO GET CAROUSEL INDEX
+
+  $(".academy-tabs__nav-tabs").trigger("to.owl.carousel", [
+    inputType.val(),
+    1,
+    true,
+  ]);
+});
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 });
