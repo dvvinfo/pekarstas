cf54db858a0222fc3e55540076d52ac080d2a9df
Vladimir Drobyshev <dvvinfo@mail.ru>
2022-10-05 16:19:50 +0300

    fix mobile academy tabs

--- js/slider_mobile.js
+++ js/slider_mobile.js
@@ -18,6 +18,22 @@ $(document).ready(function () {
     }
   );
 
+  // ============================= проверка на разакшкние экрана <= 576 ==================================
+  $(window)
+    .on("resize", function () {
+      if ($(window).width() <= 576) {
+        $("#myTabContent1").addClass("academy-slider__tab");
+        $(".academy-tabs__wrapper").addClass("academy-slider__wrapper");
+        $(".academy-tabs__nav-tabs").addClass("academy-slider");
+      } else {
+        $("#myTabContent1").removeClass("academy-slider__tab");
+        $(".academy-tabs__wrapper").removeClass("academy-slider__wrapper");
+        $(".academy-tabs__nav-tabs").removeClass("academy-slider");
+      }
+    })
+    .trigger("resize");
+
+  // ================================== мобильная карусель кнопок академии =======================
   var owl = $(".academy-slider"),
     // rangeArr = [],
     inputType = $(".slider-range__input");
@@ -28,7 +44,7 @@ $(document).ready(function () {
     autoHeight: true,
     stagePadding: 10,
   });
-  
+
   owl.on("changed.owl.carousel", function (event) {
     console.log(event.item.index);
     inputType.val(event.item.index);
@@ -40,6 +56,37 @@ $(document).ready(function () {
     // FIGURE OUT HOW TO GET CAROUSEL INDEX
 
     $(".academy-slider").trigger("to.owl.carousel", [inputType.val(), 1, true]);
+  });
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  // ========================================= мобильная карусель табов академии ========================
+  var owl = $(".academy-slider__tab"),
+    // rangeArr = [],
+    inputType = $(".slider-range__input");
+  owl.owlCarousel({
+    items: 1,
+    //loop: true,
+    margin: 8,
+    autoHeight: true,
+  });
+
+  owl.on("changed.owl.carousel", function (event) {
+    console.log(event.item.index);
+    inputType.val(event.item.index);
+  });
+  $("input").on("change", function (e) {
+    e.preventDefault();
+    console.log(inputType.val());
+    // console.log(e.item.index);
+    // FIGURE OUT HOW TO GET CAROUSEL INDEX
+
+    $(".academy-slider__tab").trigger("to.owl.carousel", [
+      inputType.val(),
+      1,
+      true,
+    ]);
   });
   $(function () {
     $('[data-toggle="tooltip"]').tooltip();
